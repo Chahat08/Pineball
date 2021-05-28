c378f7c0792501b70ff9970660bc185e9977acfb
Chahat Kalsi <chahat.ck88@gmail.com>
2021-05-29 00:19:55 +0530

    created basic flipper movement using hinge joint

--- Assets/Scripts/FlipperMovement.cs
+++ Assets/Scripts/FlipperMovement.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class FlipperMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject point;
+    public float angle = 30f;
+    private Vector3 axis = new Vector3(0, 0, 1);
+
+    public float restPosition = 0f;
+    public float pressedPosition = 45f;
+    public float hitStrength = 10000f;
+    public float flipperDamper = 150f;
+
+    HingeJoint hinge;
+
+    private void Start()
     {
-        
+        hinge = GetComponent<HingeJoint>();
+        hinge.useSpring = true;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        JointSpring spring = new JointSpring();
+        spring.spring = hitStrength;
+        spring.damper = flipperDamper;
+
+        if(Input.GetKey(KeyCode.LeftArrow)||Input.GetKey("a"))
+        {
+            spring.targetPosition = pressedPosition;
+        }
+        else
+        {
+            spring.targetPosition = restPosition;
+        }
+        hinge.spring = spring;
+        hinge.useLimits = true;
     }
 }
