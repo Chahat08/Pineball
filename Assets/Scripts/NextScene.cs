7bdddcaf6cf2a8cf99f530998b72f0aec0b6665d
Chahat Kalsi <chahat.ck88@gmail.com>
2021-05-30 14:31:05 +0530

    script for loading next scene added

---
+++ Assets/Scripts/NextScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    public void loadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
