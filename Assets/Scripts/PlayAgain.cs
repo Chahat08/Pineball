8ea7668ef9045d905ddf5c697ebed9822d0e846a
Chahat Kalsi <chahat.ck88@gmail.com>
2021-05-30 14:46:35 +0530

    closing scene added

---
+++ Assets/Scripts/PlayAgain.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayAgain : MonoBehaviour
+{
+    public void playAgain()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void instructions()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
